6b4d3f5eda0fb8385ced022ef50f4fe728ea1069
Alex Arriaga <alexzander.arriaga@base22.com>
2018-10-25 12:51:46 -0500

    Configure Stencil.js to use SASS plugin

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,14 +1,18 @@
-import { Config } from '@stencil/core';
+import {Config} from '@stencil/core';
+import {sass} from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'mycomponent',
-  outputTargets:[
+  outputTargets: [
     {
-      type: 'dist'
+      type: 'dist',
     },
     {
       type: 'www',
-      serviceWorker: null
-    }
-  ]
+      serviceWorker: null,
+    },
+  ],
+  plugins: [
+    sass(),
+  ],
 };
